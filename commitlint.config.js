93c948cc3c029ccf1d26204353edfe0a4049f350
David Sandoval <dev@sandovaldavid.com>
2026-06-24 21:38:47 -0500

    ci(ci): add commitlint with required scopes and Husky git hooks

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-empty": [2, "never"],
+    "scope-enum": [
+      2,
+      "always",
+      ["server", "plugin", "docs", "ci", "config", "deps", "release"],
+    ],
+    "scope-case": [2, "always", "lower-case"],
+    "header-max-length": [2, "always", 100],
+    "subject-full-stop": [2, "never", "."],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+  },
+};
+
+module.exports = config;
